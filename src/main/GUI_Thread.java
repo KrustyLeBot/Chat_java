419740c40e0c7ad91a1ad8b985f9818f23dae9a4
jerome <je.bravais@gmail.com>
2018-12-17 08:49:23 +0100

    ajout print console

--- src/main/GUI_Thread.java
+++ src/main/GUI_Thread.java
@@ -21,22 +21,24 @@ public class GUI_Thread implements Runnable{
 				//If the message is from the user itself(like a broadcast) exclude it
 				if(msg.getEmetteur().ip.equals(Main.local_host))return;	
 				
-				System.out.println("messaged recu de l'exterieur");
-				
 				//First, message paring and casting to the correct type
 				//If the message is a check
 				if(msg instanceof MsgCheck) {
+					
+					System.out.println("Check recu");
 					MsgCheck message = (MsgCheck) msg;	
 					
 					//If it is a message from blank_user and asking an answer
 					if(message.need_an_answer & message.getEmetteur().pseudo.equals(Main.blank.pseudo)) {
 						//Then send a check back without asking for an answer
+						System.out.println("Check avec demande de reponse");
 						Main.msg_sender.sendCheck(false, Main.me, message.getEmetteur());
 					}
 
 					//If it is check not asking an answer
 					else if(!message.need_an_answer) {
 						//Then add the user to the list of connected people
+						System.out.println("Check sans demande de reponse");
 						Main.hm_users.put(message.getEmetteur().pseudo, message.getEmetteur().ip);
 						System.out.println("User added: " + message.getEmetteur().pseudo);
 							
@@ -48,6 +50,7 @@ public class GUI_Thread implements Runnable{
 				
 				//If the message is a bye, remove the sender from the list of connected users
 				else if(msg instanceof MsgGoodbye) {
+					System.out.println("Message goodbye recu");
 					System.out.println("User removed: " + msg.getEmetteur().pseudo);
 					Main.hm_users.remove(msg.getEmetteur().pseudo);
 					
